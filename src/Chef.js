eb3965c138b351bf37dce9810b7d9093ecec2bcf
Phillip Strom <phillip.strom@gmail.com>
2021-02-19 11:12:33 -0700

    Pass all chef tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,23 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+
+  greetCustomer(name, isMorning) {
+    if (isMorning) {
+        return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+
+  checkForFood(item) {
+    if (this.restaurant.menus[item.type].includes(item)) {
+      return `Yes, we're serving ${item.name} today!`;
+    } else {
+      return `Sorry, we aren't serving ${item.name} today.`;
+    }
   }
 }
 
