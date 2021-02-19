664c0ee00ed29b68a6a52151b514a59294d598a6
Phillip Strom <phillip.strom@gmail.com>
2021-02-19 10:20:53 -0700

    Pass all meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,43 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`
+}
 
+function createMenuItem(name, price, type) {
+  return {
+    name,
+    price,
+    type
+  }
+}
+
+function addIngredients(newIngredient, ingredients) {
+  if (!ingredients.includes(newIngredient)) {
+    ingredients.push(newIngredient);
+  }
+}
+
+function formatPrice(price) {
+  return '$' + price
+}
+
+function decreasePrice(price) {
+  return price * .9;
+}
+
+function createRecipe(title, ingredients, type) {
+  return {
+    title,
+    ingredients,
+    type
+  }
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
