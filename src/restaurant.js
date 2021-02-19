6616ba8f0cdaee2131e0cdc7b22fe510bfe103ad
Phillip Strom <phillip.strom@gmail.com>
2021-02-19 10:58:46 -0700

    Pass all restaurant tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,32 @@
+function createRestaurant(name) {
+  return {
+    name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+}
 
+function addMenuItem(restaurant, item) {
+  if (!restaurant.menus[item.type].includes(item)) {
+    restaurant.menus[item.type].push(item);
+  }
+}
+
+function removeMenuItem(restaurant, item, type) {
+  for (let i = 0; i < restaurant.menus[type].length; i++) {
+    if (restaurant.menus[type][i].name === item) {
+      restaurant.menus[type].splice(i, 1)
+      return `No one is eating our ${item} - it has been removed from the ${type} menu!`
+    }
+  }
+  return `Sorry, we don't sell ${item}, try adding a new recipe!`
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
